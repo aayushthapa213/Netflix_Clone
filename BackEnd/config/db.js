5bbbb37bff25909282144caf226fc00e2b2b26fa
Aayush Thapa <aayushthapaa213@gmail.com>
2026-07-23 23:28:43 +0545

    Database Connected

---
+++ BackEnd/config/db.js
@@ -0,0 +1,13 @@
+import mongoose, { connect, mongo } from "mongoose";
+import { ENV_VARS } from "./envVars.js";
+import e from "express";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
+    console.log("MongoDB Connected: " + conn.connection.host);
+  } catch (error) {
+    process.exit(1);
+    console.log("Error Connecting to MongoDB: " + error.message);
+  }
+};
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
 import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -10,4 +11,5 @@ app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server Running on Port: ${PORT}`);
+  connectDB();
 });
