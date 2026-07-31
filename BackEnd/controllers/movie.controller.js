d2cdfe51c88e97355995ca2cb31d86c8bd9f2107
Aayush Thapa <aayushthapaa213@gmail.com>
2026-07-31 07:46:05 +0545

    getMovieDetails Controller

--- BackEnd/controllers/movie.controller.js
+++ BackEnd/controllers/movie.controller.js
@@ -33,3 +33,21 @@ export async function getMovieTrailers(req, res) {
       .json({ success: false, message: "Internal Server Error!" });
   }
 }
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+    );
+    res.json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    console.log("Error in getMovieDetails Controller: ", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error!" });
+  }
+}
