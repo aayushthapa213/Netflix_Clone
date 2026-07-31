46fd4cfc888a8cc26c18f60015a1096576bcc619
Aayush Thapa <aayushthapaa213@gmail.com>
2026-07-31 07:55:39 +0545

    getSimilarMovies Controller

--- BackEnd/routes/movie.route.js
+++ BackEnd/routes/movie.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getMovieDetails,
   getMovieTrailers,
+  getSimilarMovies,
   getTrendingMovie,
 } from "../controllers/movie.controller.js";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/trending", getTrendingMovie);
 router.get("/:id/trailers", getMovieTrailers);
 router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
 
 export default router;
