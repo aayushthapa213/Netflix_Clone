fa6b4d51affda4cf6d1847a44ffc5f31e59b94cc
Aayush Thapa <aayushthapaa213@gmail.com>
2026-08-01 23:20:15 +0545

    tv route and controller

--- BackEnd/controllers/tv.controller.js
+++ BackEnd/controllers/tv.controller.js
@@ -1,87 +1,87 @@
 import { fetchFromTMDB } from "../services/tmdb.service.js";
 
-export async function getTrendingMovie(req, res) {
+export async function getTrendingTv(req, res) {
   try {
     const data = await fetchFromTMDB(
-      "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
+      "https://api.themoviedb.org/3/trending/tv/day?language=en-US",
     );
     const randomMovie =
       data.results[Math.floor(Math.random() * data.results?.length)];
     res.status(200).json({ success: true, content: randomMovie });
   } catch (error) {
-    console.log("Error in getTrendingMovie Controller: ", error.message);
+    console.log("Error in getTrendingTv Controller: ", error.message);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error! " });
   }
 }
 
-export async function getMovieTrailers(req, res) {
+export async function getTvTrailers(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`,
     );
     res.status(200).json({ success: true, trailers: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error in getMovieTrailers Controller: ", error.message);
+    console.log("Error in getTvTrailers Controller: ", error.message);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error!" });
   }
 }
 
-export async function getMovieDetails(req, res) {
+export async function getTvDetails(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}?language=en-US`,
+      `https://api.themoviedb.org/3/tv/${id}?language=en-US`,
     );
     res.status(200).json({ success: true, content: data });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error in getMovieDetails Controller: ", error.message);
+    console.log("Error in getTvDetails Controller: ", error.message);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error!" });
   }
 }
 
-export async function getSimilarMovies(req, res) {
+export async function getSimilarTvs(req, res) {
   const { id } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`,
+      `https://api.themoviedb.org/3/tv/${id}/similar?language=en-US&page=1`,
     );
     res.status(200).json({ success: true, similar: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error in getSimilarMovie Controller: ", error.message);
+    console.log("Error in getSimilarTvs Controller: ", error.message);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error!" });
   }
 }
 
-export async function getMoviesByCategory(req, res) {
+export async function getTvsByCategory(req, res) {
   const { category } = req.params;
   try {
     const data = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`,
+      `https://api.themoviedb.org/3/tv/${category}?language=en-US&page=1`,
     );
     res.status(200).json({ success: true, content: data.results });
   } catch (error) {
     if (error.message.includes("404")) {
       return res.status(404).send(null);
     }
-    console.log("Error in getMoviesByCategory Controller: ", error.message);
+    console.log("Error in getTvsByCategory Controller: ", error.message);
     return res
       .status(500)
       .json({ success: false, message: "Internal Server Error!" });
