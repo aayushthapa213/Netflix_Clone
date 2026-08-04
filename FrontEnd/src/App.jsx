94d8911c9a3dff84b95a928bb50aaf4902c12ab4
Aayush Thapa <aayushthapaa213@gmail.com>
2026-08-05 00:00:12 +0545

    Routes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+
 function App() {
   return (
-    <>
-      <h1 class="text-3xl text-red-600 font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<LogInPage />} />
+    </Routes>
   );
 }
 
